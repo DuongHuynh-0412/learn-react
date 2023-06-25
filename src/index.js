a1b8afae05528b296c11881f93287938aba5ee16
Tiger-man5321 <100669052+Tiger-man5321@users.noreply.github.com>
2023-06-25 09:15:38 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Seed from './seed'
+
+class ProductList extends React.Component {
+    state = {
+        products: [],
+    };
+    handleProductUpVote = (productId) => {
+        const nextProducts = this.state.products.map((product) => {
+            if (product.id === productId) {
+                return Object.assign({}, product, {votes: product.votes + 1,});
+            }
+            else {
+                return product;
+            }
+        });
+        this.setState({
+            products: nextProducts
+        });
+    }
+    componentDidMount() {
+        this.setState({ products: Seed.products });
+    }
+
+    render() {
+        const products = this.state.products.sort((a, b) => (
+            b.votes - a.votes
+        ));
+        const productlist = products.map((product)=>(
+            <Product
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+                onVote = {this.handleProductUpVote}
+            />
+        ));
+        return (
+            <div className='ui unstackable items'>
+                {productlist}
+            </div>
+        );
+    }
+}
+class Product extends React.Component {
+    handleUpVote = () => (
+        this.props.onVote(this.props.id)
+    );
+    render() {
+        return (
+            <div className='item'>
+                <div className='image'>
+                    <img src={this.props.productImageUrl} />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='header'>
+                        <a onClick={this.handleUpVote}>
+                            <i className='large caret up icon' />
+                        </a>
+                        {this.props.votes}
+                    </div>
+                    <div className='description'>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img
+                            className='ui avatar image'
+                            src={this.props.submitterAvatarUrl}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <ProductList/>,
+    document.getElementById('content')
+)
